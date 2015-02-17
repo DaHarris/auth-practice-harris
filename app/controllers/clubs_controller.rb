c3a5a122c8682197c7dc5084d962fc8214796a6b
Daniel Harris <bobth3bum@gmail.com>
2015-02-17 12:20:57 -0700

    add controller functionality for clubs related to current_user

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,11 +1,23 @@
 class ClubsController < ApplicationController
 
+  before_action :require_login
+
+
+  def require_login
+    if !current_student
+      redirect_to login_path, :notice => 'You must be logged in to visit this page.'
+    end
+  end
+
   def index
-    @clubs = Club.all
+    @clubs = current_student.clubs
   end
 
   def show
     @club = Club.find(params[:id])
+    if !@club.students.find_by_id(current_student.id)
+      render 'public/403.html'
+    end
   end
 
 end
