55d2461f7ab7943e4a423eb19de03d9431673798
Daniel Harris <bobth3bum@gmail.com>
2015-02-17 12:21:44 -0700

    add student controller functionality for login before accessing students section

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,13 @@
 class StudentsController < ApplicationController
 
+  before_action :require_login
+
+  def require_login
+    if !current_student 
+      redirect_to login_path, :notice => 'You must be logged in to visit this page.'
+    end
+  end
+
   def index
     @students = Student.all
   end
